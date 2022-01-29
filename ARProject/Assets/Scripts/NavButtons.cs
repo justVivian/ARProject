1485591d17615ed7ee78302e6b1b87968e87892f
Vivian Justiana <65060988+justVivian@users.noreply.github.com>
2022-01-29 21:30:00 +0700

    Fixing Buttons in AR Scenes

--- ARProject/Assets/Scripts/NavButtons.cs
+++ ARProject/Assets/Scripts/NavButtons.cs
@@ -2,10 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class NavButtons : MonoBehaviour
 {
     // Start is called before the first frame update
+    public void OnPointerUp(PointerEventData eventData){
+        Debug.Log("CALLED");
+    }
+
     public void ExitButton(){
         Application.Quit();
         Debug.Log("Game Close");
